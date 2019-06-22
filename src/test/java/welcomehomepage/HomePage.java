9fb25c254661c6ba26d381f43b5fc67c39b38b32
weqas1994 <weqas1994@gmail.com>
2019-06-22 10:22:13 -0400

    hello my name is asel

--- src/test/java/welcomehomepage/HomePage.java
+++ src/test/java/welcomehomepage/HomePage.java
@@ -5,6 +5,7 @@ public class HomePage {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("welcome to git");
+System.out.println("hello this is asel");
 	}
 
 }

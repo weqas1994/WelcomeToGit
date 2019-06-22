1f486d6a99e7db419ba84b19ec5b98362110501e
weqas1994 <weqas1994@gmail.com>
2019-06-22 10:16:00 -0400

    welcome to git

---
+++ src/test/java/welcomehomepage/HomePage.java
@@ -0,0 +1,10 @@
+package welcomehomepage;
+
+public class HomePage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("welcome to git");
+	}
+
+}

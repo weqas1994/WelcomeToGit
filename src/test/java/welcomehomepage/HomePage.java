aa6ddab54fffda5cd9aae0166b2ed1adfc6c314f
Rasmi21 <imac@iMacs-iMac-3.local>
2019-06-22 10:23:32 -0400

    hello this arif

--- src/test/java/welcomehomepage/HomePage.java
+++ src/test/java/welcomehomepage/HomePage.java
@@ -3,8 +3,8 @@ package welcomehomepage;
 public class HomePage {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 System.out.println("welcome to git");
+System.out.println("hello my name is arif");
 	}
 
 }
